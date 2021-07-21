742c99c24ad500a67daa5e296d2f855fd63f0b20
MajestikButter <75375633+MajestikButter@users.noreply.github.com>
2021-07-21 00:12:18 -0400

    Example events in index.ts

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,15 @@
 import Events from './libs/events.js';
+import { print, runCommand } from './libs/general.js';
 
 Events.on('beforeChat', (evd) => {
-  if (evd.message.startsWith('!')) evd.cancel = true;
+  evd.cancel = true;
+  runCommand(`tellraw @a {"rawtext":[{"text":"${evd.sender.name}: ${evd.message}"}]}`);
+});
+
+Events.on('playerCreated', (evd) => {
+  print(`${evd.player.name} has loaded in`);
+});
+
+Events.on('worldStarted', (evd) => {
+  print(`The world has started after ${evd.tickStamp} ticks`);
 });
